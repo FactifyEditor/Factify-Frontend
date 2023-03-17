288aab3ded7256b076ccd4bb7b838a83d881c18b
ravi18 <ravi@arrowai.com>
2023-03-17 13:01:29 +0530

    new api server added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,9 @@
 
 export const environment = {
   production: false,
-  // BASE_URL:'https://news-checker-mwbdokzq3a-uc.a.run.app/api/v1',
+  BASE_URL:'https://factify-api-y5mm2k5uoa-em.a.run.app/api/v1',
   RENDERER_URL:'http://34.124.160.156:8080',
-  BASE_URL:'http://localhost:3000/api/v1'
+  // BASE_URL:'http://localhost:3000/api/v1'
 
 };
 
